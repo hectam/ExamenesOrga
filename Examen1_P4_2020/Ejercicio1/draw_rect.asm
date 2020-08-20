13772e08cdc6e86df302fb08a5ac3756ca245db4
hectam <41806726+hectam@users.noreply.github.com>
2020-08-19 19:35:02 -0600

    Add files via upload

--- Examen1_P4_2020/Ejercicio1/draw_rect.asm
+++ Examen1_P4_2020/Ejercicio1/draw_rect.asm
@@ -4,18 +4,27 @@
 .global draw_rect
 
 draw_rect:
-addi $sp, $sp, -4
+addi $sp, $sp, -12
 sw $ra, 0($sp)
+sw $s0, 4($sp)
+sw $s1, 8($sp)
 
 
 move $t0, $a0; width
 move $t1, $a1; height
+
 move $t2, $zero; width mod
+
 move $t3, $zero; height mod
 
 check_pos:
 
 move $t2, $zero;
+
+;salto de linea
+li $a0, 10
+li $v0, 11
+syscall
 
 beq $t3, $zero, print_H
 
@@ -56,6 +65,8 @@ addi $t2, $t2, 1
 
 bne $t2, $t0, print_V
 
+addi $t3, $t3, 1
+
 beq $t2, $t0, check_pos
 
 
@@ -72,13 +83,15 @@ addi $t2, $t2, 1
 
 bne $t2, $t0, print_H
 
-
+addi $t3, $t3, 1
 
 bne $t3, $t1, check_pos
 
 
 lw $ra, 0($sp)
-addi $sp, $sp, 4
+lw $s0, 4($sp)
+lw $s1, 8($sp)
+addi $sp, $sp, 12
 
 jr $ra
 
